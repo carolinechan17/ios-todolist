ea0d272ef0625b6921eec05efc9941ea070054c1
Caroline Chan <carolinechan304@gmail.com>
2022-08-11 15:03:59 +0700

    Initial commit

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Caroline Chan on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
